91034cbfbb511fdda2e5dcd24aa12dd4b0dc866a
Marsel Fetlyaev <fetmarsel2002@gmail.com>
2023-07-03 17:37:52 -0400

    update

--- realtime_gpt.py
+++ realtime_gpt.py
@@ -1,11 +1,13 @@
 from tkinter import *
 from netmiko import ConnectHandler
+from customtkinter import *
 import threading
 import time
 
-window = Tk()
-window.title("Realtime Interface Table Tracker")
-window.geometry("300x300")
+set_appearance_mode("Dark")
+set_default_color_theme("green")
+
+
 
 ios_l2 = {
     'device_type': 'cisco_ios',
@@ -15,62 +17,88 @@ ios_l2 = {
     'conn_timeout': 9999999
 }
 
-interface_data = []
-
 class Table:
     def __init__(self, root, data):
         for i in range(len(data)):
             for j in range(len(data[i])):
-                e = Entry(root, width=250, font=('Arial', 16, 'bold'))
+                e = CTkEntry(root, width=250, font=('Arial', 16, 'bold'))
                 e.grid(row=i, column=j)
                 e.insert(END, data[i][j])
 
-def update_window(win):
-    while True:
+
+def update_window(win,values):
+    for i in range(len(values)):
+        for j in range(len(values[0])):
+            if type(values[i][j]) == int:
+                values[i][j] += 5
+                print('hi')
+    t = Table(win, values)
+    window.after(int(update_timer.get())*1000,update_window,win,values)
+def create_window():
         try:
+            newWindow = CTkToplevel(window)
+            newWindow.title(ios_l2['ip'])
+            newWindow.geometry("750x750")
+            data_list = []
             ints_command = ssh.send_command("sh int | i up | rate")
             ints_command = ints_command.split("\n")
-            data_list = []
             for i, line in enumerate(ints_command):
                 if "Gigabit" in line:
                     rx, tx = ints_command[i+1].split()[4], ints_command[i+2].split()[4]
                     data_list.append([line[0:21], rx, tx])
-            global interface_data
-            interface_data = data_list
+            
+            t = Table(newWindow, data_list)
+            update_window(newWindow,data_list)
         except Exception as e:
             print(f"An error occurred: {str(e)}")
         
-        time.sleep(5)
 
 def newTrack():
     ios_l2['ip'] = device_ip.get()
     ios_l2['username'] = device_username.get()
     ios_l2['password'] = device_password.get()
     global ssh
-    ssh = ConnectHandler(**ios_l2)
+    #ssh = ConnectHandler(**ios_l2)
 
-    newWindow = Toplevel(window)
-    newWindow.title(ios_l2['ip'])
-    newWindow.geometry("750x750")
+    global values
+    threading.Thread(target=create_window).start()
 
-    global interface_data
-    t = Table(newWindow, interface_data)
-    
-    threading.Thread(target=update_window, args=(newWindow,)).start()
+window = CTk()
+window.title("Realtime Interface Table Tracker")
+window.geometry("300x300")
 
-greeting = Label(window, text="Realtime Interface Table Tracker")
+greeting = CTkLabel(window, text="Realtime Interface Table Tracker")
 greeting.place(relx=0.5, rely=0.1, anchor=CENTER)
 
-device_ip = Entry(window, width=120 )
+device_ip = CTkEntry(window, placeholder_text="IP Address",
+                     width=120,
+                     height=25,
+                     border_width=2,
+                     corner_radius=10)
 device_ip.place(relx=0.5, rely=0.2, anchor=CENTER)
 
-device_username = Entry(window, width=120)
+device_username = CTkEntry(window, placeholder_text="Username",
+                           width=120,
+                           height=25,
+                           border_width=2,
+                           corner_radius=10)
 device_username.place(relx=0.5, rely=0.3, anchor=CENTER)
-
-device_password = Entry(window, show="*", width=120)
+device_password = CTkEntry(window, placeholder_text="Password", show="*",
+                           width=120,
+                           height=25,
+                           
+                           border_width=2,
+                           corner_radius=10)
 device_password.place(relx=0.5, rely=0.4, anchor=CENTER)
 
-load_button = Button(window, text="Start Tracking", command=newTrack)
-load_button.place(relx=0.5, rely=0.5, anchor=CENTER)
+update_timer = CTkEntry(window, placeholder_text="Refresh Time (s)",
+                           width=120,
+                           height=25,
+                           border_width=2,
+                           corner_radius=10)
+update_timer.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+load_button = CTkButton(window, text="Start Tracking", command=newTrack)
+load_button.place(relx=0.5, rely=0.7, anchor=CENTER)
 
 window.mainloop()
